db312530f6f53fa9fac4542c012cc8ae24bef730
Rafax-boop <rafaelminon@outlook.com>
2025-09-30 15:57:11 -0600

    Primer commi t

---
+++ Metas.ApliccionWeb/Controllers/AccesoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Metas.AplicacionWeb.Controllers
+{
+    public class AccesoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(int idUsuario)
+        {
+            return View();
+        }
+    }
+}
