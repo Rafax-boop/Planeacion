235b80df057ba72eb2bc9c085c1f71c3ccf18260
Oscar <oscarpandilo@gmail.com>
2025-10-07 09:46:19 -0600

    Se inicia vista de Ejercicios de Programacion junto con pequeños detalles a la barra de navegacion

---
+++ Metas.ApliccionWeb/Controllers/ProgramacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Metas.AplicacionWeb.Controllers
+{
+    public class ProgramacionController : Controller
+    {
+        public IActionResult Programacion()
+        {
+            return View();
+        }
+    }
+}
